3a6e1700078549110916a7bff242e317f615b201
thomasewing <31376385+thomasewing@users.noreply.github.com>
2023-08-31 18:30:06 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Greetings, Salutations, & Namaste, esteemed existential terrestrial brethren!</h1>
+      </div>
+      <div>
+        <h3>Things I Need To Don't:</h3>
+      </div>
+    
+        <ul>
+          <li>Pick my nose at stop-lights</li>
+          <li>Shotgun cans of wine</li>
+          <li>Wear my flip-flops on the wrong feet</li>
+          <li>Find out the hard way if my truck will run on fruit punch instead of gasoline</li>
+        </ul>
+      
+
+    </>
+  )
+}
+
+export default App
